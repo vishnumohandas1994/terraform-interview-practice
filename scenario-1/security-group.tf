74016615e12d66e3dd18aa85646648d6fb7fd154
Vishnu Mohandas <kmvishnumohandas@gmail.com>
2026-08-16 14:28:38 +0400

    Made scenario 1 to folder

---
+++ scenario-1/security-group.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "web_sg" {
+  name        = "interview-web-sg"
+  description = "Security group for interview web server"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_cidr]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "interview-web-sg"
+  }
+}
